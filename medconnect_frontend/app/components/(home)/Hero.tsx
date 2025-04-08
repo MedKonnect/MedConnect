2f063a1d02f5125a446267d1314f1d53d1ff2f93
wheval <whevalmezeegbe@gmail.com>
2025-04-08 22:19:27 +0100

    update hero section

--- medconnect_frontend/app/components/(home)/Hero.tsx
+++ medconnect_frontend/app/components/(home)/Hero.tsx
@@ -7,13 +7,13 @@ const Hero = () => {
     <section className='mx-4 my-2 xl:mx-12 xl:my-6'>
         <div className='h-[90vh] flex flex-col justify-between text-white bg-[url("/img/bg.webp")] bg-cover bg-no-repeat px-4 lg:px-12 py-4 lg:py-8 max-w-screen-2xl rounded-[40px] mx-auto'>
             <Nav/>
-            <div className='grid lg:grid-cols-2 grid-cols-1 grid-rows-1 gap-10 pb-20 justify-between justify-items-end'>
-                <div className=' xl:mt-[-100px]'>
-                    <h1 className='text-[62px] leading-[1]'>
-                        Be connected with your health
+            <div className='flex flex-col xl:w-[60ch] gap-y-5 pb-6 justify-start'>
+                <div className=''>
+                    <h1 className='text-[62px] font-medium leading-[80px]'>
+                        Be The Center of your Health Journey
                     </h1>
                 </div>
-                <div className='lg:w-[40ch] flex flex-col gap-y-5 pr-8'>
+                <div className='lg:w-[40ch] flex flex-col gap-y-6 pr-8'>
                     <p>Access your health records, consult with doctors, and manage prescriptions—all in one place. </p>
                     <Button size='lg' variant="solid">Sign up</Button>
                 </div>
